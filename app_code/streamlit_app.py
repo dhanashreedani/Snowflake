a107a2e1a9b80ca5503b16358f80e12d3ea93261
dhanashreedani <142527630+dhanashreedani@users.noreply.github.com>
2026-05-20 14:37:14 +0530

    Update streamlit_app.py

--- app_code/streamlit_app.py
+++ app_code/streamlit_app.py
@@ -1,9 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Create Snowflake session
-
 st.title("🥤 Customize Your Smoothie! 🥤")
 
 # Description text
@@ -13,42 +13,34 @@ Choose the fruits you want in your custom Smoothie!
 """
 )
 
-
 name_on_order = st.text_input("Name on smoothie: ")
 st.write("The name on your smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
 
 Ingredients_list = st.multiselect(
-    'choose up to 5 ingradients:',
+    'choose up to 5 ingredients:',
      my_dataframe,
-     max_selections = 5
+     max_selections=5
     )
 
 if Ingredients_list:
     ingredients_string = ''
-
     for fruit_chosen in Ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """', '""" + name_on_order + """' )"""
 
     st.write(my_insert_stmt)
-    #st.stop()
 
-    time_to_insert = st.button('submit order')
-
+    time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-    
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response.json())
